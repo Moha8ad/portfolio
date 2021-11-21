0c0f24fce8c9073a3af54ed91485fb8afc290be3
Moha8ad <moha8ad@Moha8ad.local>
2021-11-21 01:47:13 +0100

    quote box updated

--- src/components/quotify-components/qt-box/qt-result.jsx
+++ src/components/quotify-components/qt-box/qt-result.jsx
@@ -3,20 +3,19 @@ import React from "react";
 import { ReactComponent as Author } from '../../../assets/owl.svg'
 
 const ResultQuote = ({ result }) => (
-    <div class="container-fluid bg-danger p-5">
-        <div class="row fs-2 fw-bold align-self-center bg-warning p-5">
-           
-        <div class="col-2">
-            <Author alt='img' />
-        </div>
-        <div class="col-auto">
-            <p> {result.author}</p>
-        </div>
-        <div class="col-auto">
-            <p>{result.quote}</p>
+    <div class="d-flex flex-wrap justify-content-center fw-bold p-4">
+        <div class="col-md-5 border border-2 border-light rounded-3 p-4 m-4">
+            <div class="col-4">
+                <Author alt='img' />
+            </div>
+            <div class="col-8 fs-5 py-4">
+                {result.author}
+            </div>
+            <div class="col-12 fs-4">
+                {result.quote}
+            </div>
         </div>
     </div>
-</div>
    
 )
 
