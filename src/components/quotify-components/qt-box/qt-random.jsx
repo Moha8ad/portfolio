4f3086bab0ba1503713c9fe89d5e5e7c402afa4f
Moha8ad <moha8ad@Moha8ad.local>
2021-11-23 12:12:32 +0100

    react app name changed

--- src/components/quotify-components/qt-box/qt-random.jsx
+++ src/components/quotify-components/qt-box/qt-random.jsx
@@ -4,48 +4,58 @@ import { ReactComponent as Author } from '../../../assets/owl.svg'
 
 const RandomQuote = ({ randomQuoteId, authorsText, quotesText }) => (
     <div class="d-flex flex-wrap justify-content-center fw-bold p-4">
-        <div class="col-md-5 border border-2 border-info rounded-3 p-4 m-4">
-            <div class="col-4">
-                <Author alt='img' />
-            </div>
-            <div class="col-6 fs-5 py-4">
+        <div class="col-md-8 border border-2 border-info rounded-3 p-4 m-4">
+            <div class='row pb-4'>
+                <div class="col-3">
+                    <Author alt='img' />
+                </div>
+                <div class="col-6 align-self-end fs-5">
                 {authorsText[randomQuoteId]}
+                </div>
             </div>
             <div class="col-12 fs-4">
                 {quotesText[randomQuoteId]}
-            </div>
+            </div>    
         </div>
-        <div class="col-md-5 border border-2 border-warning rounded-3 p-4 m-4">
-            <div class="col-4">
-                <Author alt='img' />
+        <div class="row d-flex flex-wrap justify-content-center col-12 border border-2 border-light">
+            <div class="col-md-3 border border-2 border-info rounded-3 p-4 m-4">
+                <div class='row pb-4'>
+                    <div class="col-5">
+                        <Author alt='img' />
+                    </div>
+                    <div class="col-6 align-self-end fs-auto fs-md-5">
+                    {authorsText[randomQuoteId]}
+                    </div>
+                </div>
+                <div class="col-12 fs-4">
+                    {quotesText[randomQuoteId]}
+                </div>    
             </div>
-            <div class="col-8 fs-5 py-4">
-                {authorsText[randomQuoteId+1]}
+            <div class="col-md-3 border border-2 border-info rounded-3 p-4 m-4">
+                <div class='row pb-4'>
+                    <div class="col-5">
+                        <Author alt='img' />
+                    </div>
+                    <div class="col-6 align-self-end fs-auto fs-md-5">
+                    {authorsText[randomQuoteId]}
+                    </div>
+                </div>
+                <div class="col-12 fs-4">
+                    {quotesText[randomQuoteId]}
+                </div>    
             </div>
-            <div class="col-12 fs-4">
-                {quotesText[randomQuoteId+1]}
-            </div>
-        </div>
-        <div class="col-md-5 border border-2 border-danger rounded-3 p-4 m-4">
-            <div class="col-4">
-                <Author alt='img' />
-            </div>
-            <div class="col-8 fs-5 py-4">
-                {authorsText[randomQuoteId+2]}
-            </div>
-            <div class="col-12 fs-4">
-                {quotesText[randomQuoteId+2]}
-            </div>
-        </div>
-        <div class="col-md-5  border border-2 border-success rounded-3 p-4 m-4">
-            <div class="col-4">
-                <Author alt='img' />
-            </div>
-            <div class="col-8 fs-5 py-4">
-                {authorsText[randomQuoteId+3]}
-            </div>
-            <div class="col-12 fs-4">
-                {quotesText[randomQuoteId+3]}
+            <div class="col-md-3 border border-2 border-info rounded-3 p-4 m-4">
+                <div class='row pb-4'>
+                    <div class="col-5">
+                        <Author alt='img' />
+                    </div>
+                    <div class="col-6 align-self-end fs-auto fs-md-5">
+                    {authorsText[randomQuoteId]}
+                    </div>
+                </div>
+                <div class="col-12 fs-4">
+                    {quotesText[randomQuoteId]}
+                </div>    
             </div>
         </div>
     </div>
