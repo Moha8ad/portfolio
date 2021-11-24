5b6980df5a0883c686330a8a3e37078b5388c40d
Moha8ad <moha8ad@Moha8ad.local>
2021-11-24 02:34:48 +0100

    homepage layout updated

--- src/components/quotify-components/qt-box/qt-random.jsx
+++ src/components/quotify-components/qt-box/qt-random.jsx
@@ -2,46 +2,43 @@ import React from "react";
 
 import { randomColor } from '../../all-reusable-components/random-color/random-color-component'
 
-import { ReactComponent as Author } from '../../../assets/owl.svg'
-
 const RandomQuote = ({ randomQuoteId, authorsText, quotesText }) => (
-    <div class="d-flex flex-wrap align-self-center justify-content-center fw-bold p-5">
-        <div class="col-md-8 border border-3 rounded-3 p-4 m-4">
-            <div class='row pb-4'>
-                <div class="col-3">
-                    <Author alt='img' />
+    <div class=" d-flex flex-wrap align-self-center justify-content-center fw-bold">    
+        <div class="col-12 px-4">
+            <div class='my-4 pt-2' style={{backgroundColor: randomColor}}>
+                <div class="col-3 ps-2">
+                    <img alt='img' src={`https://robohash.org/${[randomQuoteId]}?&&size=180x180`}/>
                 </div>
-                <div class="col-6 align-self-end fs-5">
+                <div class="col-auto fs-4 bg-dark ps-2 mb-2" style={{color: randomColor}}>
                 {authorsText[randomQuoteId]}
                 </div>
+                
             </div>
-            <div class="col-12 fs-4" style={{color: randomColor}}>
-                {quotesText[randomQuoteId]}
+            <div class='row'>
+            <div class="col-8 me-auto fs-2 ">
+            <i class="bi bi-quote fs-1 pe-2" style={{color: randomColor}}></i>
+            {quotesText[randomQuoteId]}
+            <span> </span>
+            <button type="button" 
+                        class="btn btn-dark"
+                        data-toggle="tooltip" data-placement="left" title="Copy"
+                    > <i class="bi bi-clipboard fs-4"></i>
+                    </button>  
             </div>
-            
-            <div class="d-flex justify-content-between pt-4">
-                <div>
-                    <button type="button" class="btn btn-lg btn-success">
-                        <i class="bi bi-share fs-4 pe-2"> </i>
-                        Inspire Others!
-                    </button>    
-                    
-                    <button type="button" 
-                        class="btn btn-lg btn-dark d-inline ms-2"
-                        data-toggle="tooltip" data-placement="right" title="Copy"
-                    >
-                        <i class="bi bi-clipboard fs-4 pe-2"> </i>
-                        
-                        Copy
-                    </button>        
+            <div class="col-auto">
+                    <button type="button" class="btn btn-primary">
+                            <i class="bi bi-quote pe-2"></i>
+                            Get Inspired!
+                    </button>
+                    <button type="button" class="btn btn-success mx-2">
+                    <i class="bi bi-share pe-2"></i>
+                    Inspire Others!
+                    </button>           
                 </div>
-                <div>
-                    <button type="button" class="btn btn-lg btn-primary">Get More Inspired!</button>                
-                </div>
-            </div>
+            </div>   
         </div>
         
-        <span class="d-block p-5" />
+        <div class="d-block p-4 m-4"/>
 
     </div>
 )
