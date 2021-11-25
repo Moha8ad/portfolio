7c0fb81248874853718c662b38518b6750d6d561
Moha8ad <moha8ad@Moha8ad.local>
2021-11-25 03:46:47 +0100

    responsive layout for homepage updated

--- src/components/quotify-components/qt-box/qt-random.jsx
+++ src/components/quotify-components/qt-box/qt-random.jsx
@@ -3,30 +3,29 @@ import React from "react";
 const RandomQuote = ({ randomQuoteId, authorsText, quotesText, handleClick, randomColor }) => ( 
     <div >    
         <div class="row m-4 fw-bold pt-5">
-            <div class='col-3 align-self-center p-4' >
-                <div class="row border border-dark border-3" style={{backgroundColor: `${randomColor}`}}>
-                    <div class="col-12 col align-self-end" style={{marginTop: "-80px"}}>
+            <div class='col-6 col-sm-5 col-lg-3 p-lg-4'>
+                <div class="row" style={{backgroundColor: `${randomColor}`}}>
+                    <div class="col-12" style={{marginTop: "-80px"}}>
                         <img alt='img' src={`https://robohash.org/${[randomQuoteId]}?&&size=180x180`}/>
                     </div>
-                    <div class="col-12 align-self-end fs-4 bg-dark" style={{color: randomColor}}>
+                    <div class="col-12 fs-4" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.534)"}}>
                     {authorsText[randomQuoteId]}
                     </div>
                 </div>
-                <div class="col-12 my-4">
-                        <button type="submit" class="btn btn-lg btn-primary" onClick={() => handleClick()}>
-                                <i class="bi bi-quote pe-2"></i>
-                                Get Inspired!
-                        </button>
-                        <button type="button" class="btn btn-lg btn-success my-2">
-                        <i class="bi bi-share pe-2"></i>
-                        Inspire Others!
-                        </button>           
-                </div>
+                <div class='row pt-4 justify-content-start'>
+                <button type="button" class="col-auto  btn btn-lg btn-outline-success">
+                        <i class="bi bi-share"></i>
+                </button>
+                <button type="submit" class="col-auto ms-2 btn btn-lg btn-outline-primary" onClick={() => handleClick()}>
+                <i class="bi bi-quote"></i>        
+                </button>
+                
+                 </div>
             </div>
-            <div class="col-9 fs-2 p-4">
+            <div class="col-12 col-sm-10 col-md-7 col-lg-8 fs-2 px-4">
                 <div class="row">
                 <div class="col-12">
-                    <i class="bi bi-quote fs-1 pe-2" style={{color: randomColor}}></i>
+                    <i class="bi bi-quote fs-1" style={{color: randomColor}}></i>
                     {quotesText[randomQuoteId]} <button 
                     type="button" 
                     class="btn btn-sm btn-outline-secondary"
