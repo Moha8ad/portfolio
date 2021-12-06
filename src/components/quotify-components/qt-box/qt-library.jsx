4ce8c7b92927f8a646d652787afc0348b617d889
Moha8ad <moha8ad@asar.local>
2021-12-06 21:09:16 +0100

    library quote box are now much more responsive!

--- src/components/quotify-components/qt-box/qt-library.jsx
+++ src/components/quotify-components/qt-box/qt-library.jsx
@@ -3,28 +3,25 @@ import React from "react";
 import './qt-library.styles.scss'
 
 const LibraryQuote = ({ randomColor, searchByName }) => ( 
-    <div class="row d-flex justify-content-center">
-    {searchByName.map((result, id) => (
-        <div class="col-10 col-sm-5 m-4"> 
-            <div class="row" >
-                <div class="col-12 border border-2 border-light rounded-1" 
-                    
-                >
-                    <div class="row d-flex justify-content-center" >
-                        <div class="col-4 d-flex justify-content-center px-2">
-                            <img 
-                                alt='img'
-                                src={`https://robohash.org/${[id]}?&&size=180x180`}
-                            />
-                           
-                        </div>
-                            <div class="p-0" style={{backgroundColor: randomColor}}>
-                                <div class="col-12 fs-4 p-2" 
-                                    style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.534)"}}>
-                                
-                                    {result.author}
-                                <div class="text-light">
-                                    {result.quote}
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            {searchByName.map((result, id) => (
+                <div class="col-10 col-sm-9 col-md-5 m-4"> 
+                    <div class="row" >
+                        <div class="col-12 border border-2 border-light rounded-1 bg-info">
+                            <div class="row d-flex justify-content-center bg-dark">
+                                <div class="col-4 col-sm-3 px-2">
+                                    <img
+                                        alt='img'
+                                        src={`https://robohash.org/${[id]}?&&size=180x180`}
+                                    />
+                                </div>
+                                <div class="col-auto d-flex align-self-end fs-4 py-2 d-none d-md-block">
+                                    <div class="px-2" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.700)"}}>
+                                        {result.author}
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-end align-self-end col-2 ms-auto">
                                     <button 
                                     type="button" 
                                     class="btn"
@@ -42,13 +39,25 @@ const LibraryQuote = ({ randomColor, searchByName }) => (
                                         <i class="bi bi-share fs-5"></i>
                                     </button>
                                 </div>
+                                <div class="row p-0" style={{backgroundColor: randomColor, minHeight: "200px"}}>
+                                    <div class="col-12 p-0" 
+                                        style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.300)"}}
+                                    >
+                                        <div class="fs-4 px-2 d-md-none" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.700)"}}>
+                                            {result.author}
+                                        </div>
+                                        <div class="text-light p-2 fs-3">
+                                            {result.quote}
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                
+            ))}
         </div>
-    ))}
     </div>
 )
 
