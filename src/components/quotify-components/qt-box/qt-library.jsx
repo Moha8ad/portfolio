294c2ab65ec236f25c17f3b27caa98b4fa84a841
Moha8ad <moha8ad@asar.local>
2021-12-06 22:50:50 +0100

    library layout for small phone updated

--- src/components/quotify-components/qt-box/qt-library.jsx
+++ src/components/quotify-components/qt-box/qt-library.jsx
@@ -9,15 +9,15 @@ const LibraryQuote = ({ randomColor, searchByName }) => (
                 <div class="col-10 col-sm-9 col-md-4 px-4 py-1 border border-secondary"> 
                     <div class="row" >
                         <div class="col-12">
-                            <div class="row d-flex justify-content-center ">
-                                <div class="col-4 col-sm-3 px-2">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-12 col-sm-3 px-2">
                                     <img
                                         alt='img'
                                         src={`https://robohash.org/${[id]}?&&size=180x180`}
                                     />
                                 </div>
                                 <div class="col-auto d-flex align-self-end fs-4 py-2 d-none d-md-block">
-                                    <div class="px-2" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.700)"}}>
+                                    <div class="px-2 bg-dark" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.700)"}}>
                                         {result.author}
                                     </div>
                                 </div>
@@ -25,7 +25,7 @@ const LibraryQuote = ({ randomColor, searchByName }) => (
                                 <div class="row p-0" style={{minHeight: "200px"}}>
                                     <div class="col-12 p-0" 
                                     >
-                                        <div class="fs-5 px-2 d-md-none" style={{color: randomColor, backgroundColor:"rgba(26, 21, 25, 0.700)"}}>
+                                        <div class="fs-5 px-2 d-md-none bg-dark">
                                             {result.author}
                                         </div>
                                         <div class="text-secondary p-2 fs-4">
