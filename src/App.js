105aa3249204d55ff78cc40d05a9c02a6b529bb1
Moha8ad <bapetnapet@gmail.com>
2021-12-10 14:04:35 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import QuotifyHome from './pages/qt-home/qt-home.component';
 import QuotifySearch from './pages/qt-search/qt-search.component';
 import QuotifyLibrary from './pages/qt-library/qt-library.component';
 import QuotifyTest from './pages/qt-test/qt-test.component';
-import Presentational from './redux/presentational';
 
 import './App.css';
 
@@ -23,8 +22,8 @@ function App() {
         <Route path='/profile/search' component={QuotifySearch} />
         <Route path='/profile/library' component={QuotifyLibrary} />
         <Route path='/profile/app3' component={QuotifyTest} />
-        <Route path='/profile/app4' component={Presentational} />
-      </Switch>
+  ]
+        </Switch>
     </div>
   )
 }
