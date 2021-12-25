e5c1c14ffcee4f4238704e7bf7ad9b7dc80eba44
Moha8ad <bapetnapet@gmail.com>
2021-12-25 18:17:00 +0100

    routing setting updated

--- src/pages/move.component.jsx
+++ src/pages/move.component.jsx
@@ -1,54 +1,85 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class Liked extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cards: [
-                {
-                    card: 'CARD ONE'
-                },
-                {
-                    card: 'CARD TWO'
-                },
-                {
-                    card: 'CARD THREE'
-                }
-            ],
-            likedCards: []
-        }
-    }
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
 
-    handleLikedCards = () => {
-        const card = this.state.cards.map(card => card)
-        this.setState({
-            likedCards: [this.state.likedCards + card[0]]
-        })
-    }
+export default function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
 
-    render() {
+        <hr />
 
-        return(
-            <div class="container-fluid">
-                <div className="row">
-                    <div className="col-12 d-flex justify-content-center bg-dark text-light p-4">
-                        <div>
-                            {this.state.cards.map((card, id) => 
-                                <div key={id} class="bg-primary text-center p-4 m-4" onClick={this.handleLikedCards}>{card.card}{id}</div>
-                            )}
-                        </div>
-                    </div>
-                    <div className="col-12 bg-success text-light p-4">
-                        <div className="col-12 fs-2">Liked Cards:</div>
-                        <div className="col12">
-                            {this.state.likedCards}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Liked;
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
