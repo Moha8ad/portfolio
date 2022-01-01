922985f27d8681a5e5ecd8d77642d54cfe7f489a
Moha8ad <bapetnapet@gmail.com>
2022-01-01 01:19:24 +0100

    redirect updated

--- src/App.js
+++ src/App.js
@@ -13,33 +13,35 @@ import SignInAndSignUp from './pages/qt-account/qt-account.component';
 
 import './App.css';
 
-const App = ({ currentUser }) => {
-  return(
-    <div>      
-      <Switch>  
-        <Route exact path='/profile/' component={HomePage} />
-        <Route path='/profile/rolodex' component={Rolodex} />
-        <Route path='/profile/quotify' component={QuotifyMain} />
-        <Route path='/profile/search' component={QuotifyMain} />
-        <Route path='/profile/library' component={QuotifyMain} />
-        <Route path='/profile/snakes-and-ladders' component={SnakesAndLadders} />
-        <Route path='/profile/app4' component={Presentational} />
-        <Route path='/profile/app5' component={Move} />
-        <Route exact path='/profile/account' 
-          render={() => currentUser ? (
-            <Redirect to='/profile/quotify'/>
-            ) : (
-              <SignInAndSignUp />
-            )
-          }
-        />
-      </Switch>
-    </div>
-  )
-}
+const App = ({ currentUser }) => (
+  
+      <div>      
+        <Switch>  
+          <Route exact path='/profile/' component={HomePage} />
+          <Route path='/profile/rolodex' component={Rolodex} />
+          <Route path='/profile/quotify' component={QuotifyMain} />
+          <Route path='/profile/search' component={QuotifyMain} />
+          <Route path='/profile/library' component={QuotifyMain} />
+          <Route path='/profile/snakes-and-ladders' component={SnakesAndLadders} />
+          <Route path='/profile/app4' component={Presentational} />
+          <Route path='/profile/app5' component={Move} />
+          <Route 
+             path='/profile/account'
+              render={() =>
+                currentUser ? (
+                  <Redirect to='/profile/quotify' />
+                ) : (
+                  <SignInAndSignUp />
+                )
+              }
+          />
+        </Switch>
+      </div>
 
-const mapStateToProps = state => ({
+)
+
+const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser
-})
+});
 
 export default connect(mapStateToProps)(App);
