d79d51141650249e6f96fa1da1b85fb75ce3474d
Moha8ad <bapetnapet@gmail.com>
2022-01-29 16:09:41 +0100

    minor changes on homepage

--- src/components/homepage-components/directory/directory.component.jsx
+++ src/components/homepage-components/directory/directory.component.jsx
@@ -11,14 +11,14 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'Quotify',
-          subtitle: 'Get Inspired!',
+          subtitle: 'responsive',
           imageUrl: 'https://illlustrations.co/static/35e1a45f1b256f0a7f6dfac84e1675c0/day100-the-snail.png',
           linkUrl: 'qt/home',
           id: 1
         },
         {
           title: 'Snakes and Ladders',
-          subtitle: 'Game of Chance!',
+          subtitle: 'full screen desktop',
           imageUrl: 'https://illlustrations.co/static/a8bffeb0395117a5c2d54c770d50e80e/day2-gaming-console.png',
           linkUrl: 'snakes-and-ladders',
           id: 2
