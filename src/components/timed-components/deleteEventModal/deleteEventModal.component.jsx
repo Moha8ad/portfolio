a2dd1ee994eb25ab572d303d27b52fe527cc5ebf
Moha8ad <bapetnapet@gmail.com>
2022-03-25 10:22:08 +0100

    minor style changes related to Timed

--- src/components/timed-components/deleteEventModal/deleteEventModal.component.jsx
+++ src/components/timed-components/deleteEventModal/deleteEventModal.component.jsx
@@ -1,14 +1,14 @@
 const DeleteEventModal = ({ onDelete, eventText, onClose }) => {
     return ( 
-        <>
+        <div>
             <div id="timed-deleteEventModal">
-                <h2>Event</h2>
+                <h6><i>Your Event for Today</i></h6>
                 <p id="timed-eventText">{eventText}</p>
                 <button onClick={onDelete} className='timed-button' id="timed-deleteButton">Delete</button>
                 <button onClick={onClose} className='timed-button' id="timed-closeButton">Close</button>
             </div>
             <div id="timed-modalBackDrop"></div>
-        </>
+        </div>
     );
 }
  
--- src/components/timed-components/newEventModal/newEventModal.component.jsx
+++ src/components/timed-components/newEventModal/newEventModal.component.jsx
@@ -6,41 +6,43 @@ const NewEventModal = ({ onSave, onClose }) => {
     const [error, setError] = useState(false);
 
     return ( 
-        <div id="timed-newEventModal">
-            <h3>New Event</h3>
-            <input
-                className={error ? 'timed-error' : ''}
-                type="text"
-                id="timed-eventTitleInput" 
-                placeholder="Event Title " 
-                value={title}
-                onChange={e => setTitle(e.target.value )}
-            />
+        <div>
+            <div id="timed-newEventModal">
+                <h3>New Event</h3>
+                <input
+                    className={error ? 'timed-error' : ''}
+                    type="text"
+                    id="timed-eventTitleInput"
+                    placeholder="Event Title "
+                    value={title}
+                    onChange={e => setTitle(e.target.value)} />
 
-            <button 
-                className='timed-button' 
-                id="timed-saveButton"
-                onClick={() => {
-                    if (title) {
-                        setError(false);
-                        onSave(title);
-                    } else {
-                        setError(true);
-                    }
-                }}
-            >
-                Save
-            </button>
+                <button
+                    className='timed-button'
+                    id="timed-saveButton"
+                    onClick={() => {
+                        if (title) {
+                            setError(false);
+                            onSave(title);
+                        } else {
+                            setError(true);
+                        }
+                    } }
+                >
+                    Save
+                </button>
 
-            <button 
-                className='timed-button' 
-                id="timed-cancelButton"
-                onClick={onClose}
-            >
-                Cancel
-            </button>
-            
+                <button
+                    className='timed-button'
+                    id="timed-cancelButton"
+                    onClick={onClose}
+                >
+                    Cancel
+                </button>
+            </div>
+            <div id="timed-modalBackDrop"></div>
         </div>
+
     );
 }
  
