a82dc1d60f732b5c67e18088748c6b78c7d852e6
Moha8ad <bapetnapet@gmail.com>
2022-04-05 00:30:47 +0200

    testRunner path changed from hardcoded fixed path and TDD is ready to go now

--- src/components/g5-components/g5-word-box/g5-word-box.utils.js
+++ src/components/g5-components/g5-word-box/g5-word-box.utils.js
@@ -1,20 +1,13 @@
 export const highlight = ( example, word ) => {
 
+    const wordToLoweCase = word.toLowerCase();
     const exampleArr = example.toLowerCase().split(/([\wäöüÄÖÜß]+)/ig);
-    const indexOfWord = exampleArr.indexOf(word.toLowerCase());
+    const indexOfWord = exampleArr.indexOf(wordToLoweCase);
 
     const firstPartOfExample = exampleArr.slice(0, indexOfWord).join(' ');
     const secondPartOfExample = exampleArr.slice(indexOfWord + 1, exampleArr.length).join(' ');
     const wordOfExample = example.split(/([\wäöüÄÖÜß]+)/ig).slice(indexOfWord, indexOfWord + 1);
     
-    console.log(word)
-    console.log(example)
-    console.log(exampleArr)
-    console.log(indexOfWord)
-    console.log(firstPartOfExample)
-    console.log(secondPartOfExample)
-    console.log(wordOfExample)
-    console.log(example.toLowerCase().includes(word.toLowerCase()))
 
     return (
         wordOfExample.length > 0
